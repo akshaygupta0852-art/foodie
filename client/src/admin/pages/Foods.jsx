96634edccf063ccea7ba1d26ec7cb22177f4d08c
Akshay Dadarya <akshaygupta0852@gmail.com>
2026-08-09 12:05:02 +0530

    Added back function to foodpage

--- client/src/admin/pages/Foods.jsx
+++ client/src/admin/pages/Foods.jsx
@@ -1,14 +1,15 @@
 import { ArrowLeft } from 'lucide-react'
 import useDashboard from '../hooks/useDashboard'
 import FoodCard from '../components/manageFoods/FoodCard';
-
+import { useNavigate } from 'react-router-dom'
 const Foods = () => {
     const { foods } = useDashboard();
+    const navigate = useNavigate();
     return (
         <div>
             <header className='mb-2'>
                 <div className='flex px-4 items-center h-18 gap-5'>
-                    <ArrowLeft size={30} className='hover:bg-gray-200 cursor-pointer transition duration-100' />
+                    <ArrowLeft onClick={()=>navigate(-1)} size={30} className='hover:bg-gray-200 cursor-pointer transition duration-100' />
                     <div>
                         <h2 className='text-2xl font-semibold'>Manage foods</h2>
                         <p>Update and delete your Foods</p>
